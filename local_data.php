bbea88e21e803d542bf9e226c6095bda6daf09d3
zaid1982 <hemppok.kembong@gmail.com>
2019-02-22 00:30:41 +0800

    Update local_data.php

--- local_data.php
+++ local_data.php
@@ -1,13 +1,16 @@
 <?php
+require_once 'library/constant.php';
 require_once 'function/db.php';
 require_once 'function/f_general.php';
 require_once 'function/f_login.php';
 
+$constant = new Class_constant();
 $fn_general = new Class_general();
 $fn_login = new Class_login();
 $api_name = 'api_local_data';
 $is_transaction = false;
 $form_data = array('success'=>false, 'result'=>'', 'error'=>'', 'errmsg'=>'');
+$result = '';
 
 /* Error code range - 2300 */ 
 try {   
@@ -17,13 +20,13 @@ try {
     
     $headers = apache_request_headers();
     if (!isset($headers['Authorization'])) {
-        throw new Exception('(ErrCode:2301) [' . __LINE__ . '] - Parameter Authorization emtpy');
+        throw new Exception('(ErrCode:2301) [' . __LINE__ . '] - Parameter Authorization empty');
     }
     $jwt_data = $fn_login->check_jwt($headers['Authorization']);
     
     if ('GET' === $request_method) { 
         if (!isset($headers['Name']) || empty($headers['Name'])) {
-            throw new Exception('(ErrCode:2302) [' . __LINE__ . '] - Parameter Name emtpy');
+            throw new Exception('(ErrCode:2302) [' . __LINE__ . '] - Parameter Name empty');
         }
         $name = $headers['Name'];    
             
@@ -50,52 +53,47 @@ try {
                 array_push($result, $row_result);
             }  
         }
-        else if ($name === 'jenisPerniagaan') {
-            $arr_dataLocal = Class_db::getInstance()->db_select('ref_jenis_perniagaan');        
-            foreach ($arr_dataLocal as $dataLocal) {
-                $row_result = array('jenisPerniagaanId'=>'', 'jenisPerniagaanDesc'=>'', 'jenisPerniagaanStatus'=>'');
-                $row_result['jenisPerniagaanId'] = $dataLocal['jenis_perniagaan_id'];
-                $row_result['jenisPerniagaanDesc'] = $dataLocal['jenis_perniagaan_desc'];
-                $row_result['jenisPerniagaanStatus'] = $dataLocal['jenis_perniagaan_status'];
-                array_push($result, $row_result);
-            }  
-        }
         else if ($name === 'city') {
-            $arr_dataLocal = Class_db::getInstance()->db_select('ref_city');        
+            $arr_dataLocal = Class_db::getInstance()->db_select('ref_city');
             foreach ($arr_dataLocal as $dataLocal) {
-                $row_result = array('cityId'=>'', 'citiDesc'=>'', 'stateId'=>'', 'cityStatus'=>'');
+                $row_result = array('cityId' => '', 'citiDesc' => '', 'stateId' => '', 'cityStatus' => '');
                 $row_result['cityId'] = $dataLocal['city_id'];
                 $row_result['cityDesc'] = $dataLocal['city_desc'];
                 $row_result['stateId'] = $dataLocal['state_id'];
                 $row_result['cityStatus'] = $dataLocal['city_status'];
                 array_push($result, $row_result);
-            }  
-        } 
-        else if ($name === 'location') {
-            $arr_dataLocal = Class_db::getInstance()->db_select('sys_location');        
-            foreach ($arr_dataLocal as $dataLocal) {
-                $row_result = array('locationId'=>'', 'locationDesc'=>'', 'locationLongitude'=>'', 'locationLatitude'=>'', 'locationStatus'=>'');
-                $row_result['locationId'] = $dataLocal['location_id'];
-                $row_result['locationDesc'] = $dataLocal['location_desc'];
-                $row_result['locationLongitude'] = $dataLocal['location_longitude'];
-                $row_result['locationLatitude'] = $dataLocal['location_latitude'];
-                $row_result['locationStatus'] = $dataLocal['location_status'];
-                array_push($result, $row_result);
-            }  
+            }
         }
-        else if ($name === 'company') {
-            $arr_dataLocal = Class_db::getInstance()->db_select('dt_sem_company');        
+        else if ($name === 'problemtype') {
+            $arr_dataLocal = Class_db::getInstance()->db_select('icn_problemtype');
             foreach ($arr_dataLocal as $dataLocal) {
-                $row_result = array('companyId'=>'', 'companyName'=>'', 'companyNoDaftar'=>'', 'jenisPerniagaanId'=>'', 'stateId'=>'', 'companyEmail'=>'', 'statusId'=>'');
-                $row_result['companyId'] = $dataLocal['company_id'];
-                $row_result['companyName'] = $fn_general->clear_null($dataLocal['company_name']);
-                $row_result['companyNoDaftar'] = $fn_general->clear_null($dataLocal['company_ssm_no']);
-                $row_result['jenisPerniagaanId'] = $fn_general->clear_null($dataLocal['jenis_perniagaan_id']);
-                $row_result['stateId'] = $fn_general->clear_null($dataLocal['state_id']);
-                $row_result['companyEmail'] = $fn_general->clear_null($dataLocal['company_email']);
-                $row_result['statusId'] = $fn_general->clear_null($dataLocal['company_status']);
+                $row_result = array('problemtypeId' => '', 'problemtypeDesc' => '', 'problemtypeStatus' => '');
+                $row_result['problemtypeId'] = $dataLocal['problemtype_id'];
+                $row_result['problemtypeDesc'] = $dataLocal['problemtype_desc'];
+                $row_result['problemtypeStatus'] = $dataLocal['problemtype_status'];
                 array_push($result, $row_result);
-            }  
+            }
+        }
+        else if ($name === 'worktype') {
+            $arr_dataLocal = Class_db::getInstance()->db_select('icn_worktype');
+            foreach ($arr_dataLocal as $dataLocal) {
+                $row_result = array('worktypeId'=>'', 'worktypeDesc'=>'', 'worktypeStatus'=>'');
+                $row_result['worktypeId'] = $dataLocal['worktype_id'];
+                $row_result['worktypeDesc'] = $dataLocal['worktype_desc'];
+                $row_result['worktypeStatus'] = $dataLocal['worktype_status'];
+                array_push($result, $row_result);
+            }
+        }
+        else if ($name === 'workcategory') {
+            $arr_dataLocal = Class_db::getInstance()->db_select('icn_workcategory');
+            foreach ($arr_dataLocal as $dataLocal) {
+                $row_result = array('workcategoryId'=>'', 'workcategoryDesc'=>'', 'worktypeId'=>'', 'workcategoryStatus'=>'');
+                $row_result['workcategoryId'] = $dataLocal['workcategory_id'];
+                $row_result['workcategoryDesc'] = $dataLocal['workcategory_desc'];
+                $row_result['worktypeId'] = $dataLocal['worktype_id'];
+                $row_result['workcategoryStatus'] = $dataLocal['workcategory_status'];
+                array_push($result, $row_result);
+            }
         } else {
             throw new Exception('(ErrCode:2303) [' . __LINE__ . '] - Parameter name invalid ('.$name.')');
         }
@@ -116,7 +114,7 @@ try {
     if ($ex->getCode() === 31) {
         $form_data['errmsg'] = substr($ex->getMessage(), strpos($ex->getMessage(), '] - ') + 4);
     } else {
-        $form_data['errmsg'] = 'Berlaku kesilapan pada sistem. Sila hubungi pihak Admin!';
+        $form_data['errmsg'] = $constant::ERR_DEFAULT;
     }
     $fn_general->log_error($api_name, __LINE__, $ex->getMessage());
 }
